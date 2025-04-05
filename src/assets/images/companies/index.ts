2c38e94b8cdb532dd97348d2a04eae274d278c31
ABDALLAH-ATGUIRI <abdallah.atguiri@gmail.com>
2025-04-05 03:49:43 +0000

    Deploy optimized build

--- src/assets/images/companies/index.ts
+++
@@ -1,5 +0,0 @@
-import agriwiseLogo from "./agriwise-logo.png";
-import digitalEliteLogo from "./digital-elite-logo.png";
-import codNetworkLogo from "./cod-network-logo.jpg";
-
-export { agriwiseLogo, digitalEliteLogo, codNetworkLogo };
--- src/types/project.ts
+++
@@ -1,9 +0,0 @@
-interface Project {
-    images?: Array<string>;
-    key: string;
-    technologies: string[];
-    github?: string; // Optional property for GitHub URL
-    demo?: string; // Optional property for demo URL
-}
-
-export type { Project };
